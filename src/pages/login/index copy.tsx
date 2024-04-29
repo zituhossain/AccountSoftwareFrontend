2cb7db25d603c1905b8d5f40aaa613914d002fb2
zituhossain5 <zituhossain@gmail.com>
2024-04-29 14:50:05 +0600

    refurnish the login page

--- src/pages/login/index copy.tsx
+++ src/pages/login/index copy.tsx
@@ -100,8 +100,8 @@ const schema = yup.object().shape({
 })
 
 const defaultValues = {
-  password: '',
-  email: ''
+  password: 'admin',
+  email: 'admin@materialize.com'
 }
 
 interface FormData {
@@ -144,8 +144,21 @@ const LoginPage = () => {
     })
   }
 
+  const imageSource = skin === 'bordered' ? 'auth-v2-login-illustration-bordered' : 'auth-v2-login-illustration'
+
   return (
-    <Box className='content-center'>
+    <Box className='content-right'>
+      {!hidden ? (
+        <Box sx={{ flex: 1, display: 'flex', position: 'relative', alignItems: 'center', justifyContent: 'center' }}>
+          <LoginIllustrationWrapper>
+            <LoginIllustration
+              alt='login-illustration'
+              src={`/images/pages/${imageSource}-${theme.palette.mode}.png`}
+            />
+          </LoginIllustrationWrapper>
+          <FooterIllustrationsV2 />
+        </Box>
+      ) : null}
       <RightWrapper sx={skin === 'bordered' && !hidden ? { borderLeft: `1px solid ${theme.palette.divider}` } : {}}>
         <Box
           sx={{
@@ -246,7 +259,14 @@ const LoginPage = () => {
               <TypographyStyled variant='h5'>{`Welcome to ${themeConfig.templateName}! 👋🏻`}</TypographyStyled>
               <Typography variant='body2'>Please sign-in to your account and start the adventure</Typography>
             </Box>
-
+            <Alert icon={false} sx={{ py: 3, mb: 6, ...bgColors.primaryLight, '& .MuiAlert-message': { p: 0 } }}>
+              <Typography variant='caption' sx={{ mb: 2, display: 'block', color: 'primary.main' }}>
+                Admin: <strong>admin@materialize.com</strong> / Pass: <strong>admin</strong>
+              </Typography>
+              <Typography variant='caption' sx={{ display: 'block', color: 'primary.main' }}>
+                Client: <strong>client@materialize.com</strong> / Pass: <strong>client</strong>
+              </Typography>
+            </Alert>
             <form noValidate autoComplete='off' onSubmit={handleSubmit(onSubmit)}>
               <FormControl fullWidth sx={{ mb: 4 }}>
                 <Controller
@@ -261,7 +281,7 @@ const LoginPage = () => {
                       onBlur={onBlur}
                       onChange={onChange}
                       error={Boolean(errors.email)}
-                      placeholder='top4logistics@gmail.com'
+                      placeholder='admin@materialize.com'
                     />
                   )}
                 />
@@ -328,6 +348,49 @@ const LoginPage = () => {
                 <Typography href='/register' component={Link} sx={{ color: 'primary.main', textDecoration: 'none' }}>
                   Create an account
                 </Typography>
+              </Box>
+              <Divider
+                sx={{
+                  '& .MuiDivider-wrapper': { px: 4 },
+                  mt: theme => `${theme.spacing(5)} !important`,
+                  mb: theme => `${theme.spacing(7.5)} !important`
+                }}
+              >
+                or
+              </Divider>
+              <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                <IconButton
+                  href='/'
+                  component={Link}
+                  sx={{ color: '#497ce2' }}
+                  onClick={(e: MouseEvent<HTMLElement>) => e.preventDefault()}
+                >
+                  <Icon icon='mdi:facebook' />
+                </IconButton>
+                <IconButton
+                  href='/'
+                  component={Link}
+                  sx={{ color: '#1da1f2' }}
+                  onClick={(e: MouseEvent<HTMLElement>) => e.preventDefault()}
+                >
+                  <Icon icon='mdi:twitter' />
+                </IconButton>
+                <IconButton
+                  href='/'
+                  component={Link}
+                  onClick={(e: MouseEvent<HTMLElement>) => e.preventDefault()}
+                  sx={{ color: theme => (theme.palette.mode === 'light' ? '#272727' : 'grey.300') }}
+                >
+                  <Icon icon='mdi:github' />
+                </IconButton>
+                <IconButton
+                  href='/'
+                  component={Link}
+                  sx={{ color: '#db4437' }}
+                  onClick={(e: MouseEvent<HTMLElement>) => e.preventDefault()}
+                >
+                  <Icon icon='mdi:google' />
+                </IconButton>
               </Box>
             </form>
           </BoxWrapper>
